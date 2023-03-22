d5e01a03a7fd2c8b9da8f5d736740bc3a9bac1d3
Dmitry <d.myachn@gmail.com>
2023-03-22 16:19:30 +0300

    done

--- headerAvatar/ViewController.swift
+++ headerAvatar/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    lazy var scroll:UIScrollView = {
+        let view = UIScrollView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentSize = CGSize(width: self.view.frame.width, height: 1800)
+        view.backgroundColor = .systemBackground
+        return view
+    }()
+    
+    lazy var image: UIImageView = {
+        let view = UIImageView(image: UIImage(systemName: "person.crop.circle.fill"))
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.contentMode = .scaleAspectFit
+        view.tintColor = .lightGray
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Avatar"
+        
+        view.addSubview(scroll)
+        scroll.frame = view.frame
+        
+        if let largeTitleClass = NSClassFromString("_UINavigationBarLargeTitleView") as? UIView.Type {
+            if let largeTitleView = self.findSubview(parentView: self.navigationController!.view, type: largeTitleClass) {
+                guard let textView = self.findSubview(parentView: largeTitleView, type: UILabel.self) else { return }
+                
+                largeTitleView.addSubview(image)
+                
+                NSLayoutConstraint.activate([
+                    image.widthAnchor.constraint(equalToConstant: 36),
+                    image.heightAnchor.constraint(equalToConstant: 36),
+                    image.centerYAnchor.constraint(equalTo: textView.centerYAnchor),
+                    image.trailingAnchor.constraint(equalTo: largeTitleView.trailingAnchor, constant: -10),
+                ])
+            }
+        }
     }
-
-
+    
+    
+    func findSubview(parentView: UIView, type: UIView.Type) -> UIView? {
+        for subview in parentView.subviews {
+            if subview.isKind(of: type) {
+                return subview
+            } else if let desiredInstance = findSubview(parentView: subview, type: type) {
+                return desiredInstance
+            }
+        }
+        return nil
+    }
+    
 }
 
